3c3c2ddf21bec76ded7e2cacec2db162549278fe
Brian Northan <bnorthan@gmail.com>
2020-05-28 07:04:43 -0700

    Add javacppbuild.sh
    This calls the native build, calls mvn to build the jni wrapper and
    copies the wrapper to the resources folder.

---
+++ javacppbuild.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# this script builds the native part of clij2-fft
+
+# call the script that builds the c libraries
+cd native
+source cppbuild.sh
+cd ..
+
+# now call mvn as part of the maven build the javacpp wrapper will be generated
+mvn
+
+# copy the javacpp wrapper to the resource folder
+case $PLATFORM in
+    linux-x86_64)
+       	echo "TODO"
+      	;;
+    macosx-*)
+       	echo "TODO"
+       	;;
+    windows-x86_64)
+       	echo "copy jni wriapper"
+	cp target/classes/net/haesleinhuepf/clijx/plugins/windows-x86_64/jniclij2fftWrapper.dll src/resources/native/lib/
+	;;
+    *)
+        echo "Error: Platform \"$PLATFORM\" is not supported"
+        ;;
+esac
+
