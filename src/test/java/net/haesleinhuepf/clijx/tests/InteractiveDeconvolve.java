2e176f25a9712f74d810ec41201092b5c90c5499
Brian Northan <bnorthan@gmail.com>
2020-09-06 14:07:11 -0400

    Tweak some variable names

--- src/test/java/net/haesleinhuepf/clijx/tests/InteractiveDeconvolve.java
+++ src/test/java/net/haesleinhuepf/clijx/tests/InteractiveDeconvolve.java
@@ -2,26 +2,19 @@
 package net.haesleinhuepf.clijx.tests;
 
 import java.io.IOException;
-import java.util.Random;
 
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij2.CLIJ2;
 import net.haesleinhuepf.clijx.plugins.DeconvolveRichardsonLucyFFT;
 import net.haesleinhuepf.clijx.plugins.Normalize;
+import net.haesleinhuepf.clijx.plugins.OpenCLFFTUtility;
 import net.imagej.Dataset;
 import net.imagej.ImageJ;
-import net.imagej.ops.Ops;
-import net.imagej.ops.filter.pad.DefaultPadInputFFT;
-import net.imagej.ops.filter.pad.DefaultPadShiftKernelFFT;
-import net.imglib2.FinalDimensions;
-import net.imglib2.Interval;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
-import net.imglib2.outofbounds.OutOfBoundsMirrorFactory;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
-import net.imglib2.util.Intervals;
 import net.imglib2.view.Views;
 
 public class InteractiveDeconvolve<T extends RealType<T> & NativeType<T>> {
@@ -37,7 +30,6 @@ public class InteractiveDeconvolve<T extends RealType<T> & NativeType<T>> {
 		// launch IJ so we can interact with the inputs and outputs
 		ij.launch(args);
 
-
 		CLIJ2 clij2=null;
 		// get clij
 		try {
@@ -45,18 +37,18 @@ public class InteractiveDeconvolve<T extends RealType<T> & NativeType<T>> {
 		}
 		catch(Exception e) {
 			System.out.println(e);
+			return;
 		}
 
 		// load data
-		Dataset testData = (Dataset) ij.io().open("/home/bnorthan/code/images/Bars-G10-P15-stack-cropped.tif");
-		Dataset psf = (Dataset) ij.io().open("/home/bnorthan/code/images/PSF-Bars-stack-cropped-64.tif");
-		//Dataset testData = (Dataset) ij.io().open("C:/structure/data/Deconvolution_Brian/Bars-G10-P15-stack-cropped.tif");
-		//Dataset psf = (Dataset) ij.io().open("C:/structure/data/Deconvolution_Brian/PSF-Bars-stack-cropped-64.tif");
+		//Dataset testData = (Dataset) ij.io().open("/home/bnorthan/code/images/Bars-G10-P15-stack-cropped.tif");
+		//Dataset psf = (Dataset) ij.io().open("/home/bnorthan/code/images/PSF-Bars-stack-cropped-64.tif");
+		Dataset testData = (Dataset) ij.io().open("C:/structure/data/Deconvolution_Brian/Bars-G10-P15-stack-cropped.tif");
+		Dataset psf = (Dataset) ij.io().open("C:/structure/data/Deconvolution_Brian/PSF-Bars-stack-cropped-64.tif");
 
 		//Dataset testData = (Dataset) ij.io().open("C:\\Users\\bnort\\ImageJ2018\\ops-experiments\\images/Bars-G10-P15-stack-cropped.tif");
 		//Dataset psf = (Dataset) ij.io().open("C:\\Users\\bnort\\ImageJ2018\\ops-experiments\\images/PSF-Bars-stack-cropped-64.tif");
 		
-	
 		// convert input data to float
 		RandomAccessibleInterval<FloatType> imgF = (RandomAccessibleInterval) (ij
 			.op().convert().float32((Img) testData.getImgPlus().getImg()));
@@ -71,27 +63,21 @@ public class InteractiveDeconvolve<T extends RealType<T> & NativeType<T>> {
 		// crop PSF - the image will be extended using PSF size
 		// if the PSF size is too large it will explode image size, memory needed and processing speed
 		// so crop just small enough to capture significant signal of PSF 
-	//	psfF = ImageUtility.cropSymmetric(psfF,
-	//			new long[] { 64, 64, 41 }, ij.op());
-		
-		ij.ui().show(Views.zeroMin(psfF));
+		//	psfF = ImageUtility.cropSymmetric(psfF,
+		//			new long[] { 64, 64, 41 }, ij.op());
+		// ij.ui().show(Views.zeroMin(psfF));
 		
 		ClearCLBuffer gpu_psf = clij2.push(psfF);
-		ClearCLBuffer gpu_psf_normalized = clij2.create(gpu_psf);
-
-		// noremalize PSF so that its values are between 0 and 1
-		Normalize.normalize(clij2, gpu_psf, gpu_psf_normalized);
-
 		ClearCLBuffer gpu_image = clij2.push(imgF);
 		ClearCLBuffer gpu_deconvolved = clij2.create(gpu_image);
 
 		// deconvolve the image
-		DeconvolveRichardsonLucyFFT.deconvolveRichardsonLucyFFT(clij2, gpu_image, gpu_psf_normalized, gpu_deconvolved, 100);
+		DeconvolveRichardsonLucyFFT.deconvolveRichardsonLucyFFT(clij2, gpu_image, gpu_psf, gpu_deconvolved, 100);
 
-		RandomAccessibleInterval outputRAI = clij2.pullRAI(gpu_deconvolved);
+		RandomAccessibleInterval deconvolvedRAI = clij2.pullRAI(gpu_deconvolved);
 
-		//clij2.show(output, "output");
-		ij.ui().show("output", outputRAI);
+		clij2.show(deconvolvedRAI, "deconvolved");
+		//ij.ui().show("deconvolved", deconvolvedRAI);
 
 	}
 }
