50130296e662f7f35cf1fb2136f3011b2920ebd5
Brian Northan <bnorthan@gmail.com>
2020-09-06 14:00:22 -0400

    Add pad, crop and normalize utility functions
    padFFTInputMirror - pads image using mirror strategy
    
    padFFTInputZeros - pad image using zeros
    
    cropExtended - crop an extended iamge
    
    normalize - normalize so that sum is 1

--- src/main/java/net/haesleinhuepf/clijx/plugins/OpenCLFFTUtility.java
+++ src/main/java/net/haesleinhuepf/clijx/plugins/OpenCLFFTUtility.java
@@ -5,6 +5,7 @@ import net.haesleinhuepf.clij2.CLIJ2;
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij.coremem.enums.NativeTypeEnum;
 import net.imagej.ops.OpService;
+import net.imagej.ops.filter.fftSize.NextSmoothNumber;
 import net.imagej.ops.filter.pad.DefaultPadInputFFT;
 import net.imglib2.Dimensions;
 import net.imglib2.FinalDimensions;
@@ -172,7 +173,103 @@ public class OpenCLFFTUtility {
 		return ArrayImgs.complexFloats(temp, new long[] { in.dimension(0) / 2, in
 			.dimension(1) });
 	}
+	
+	
+	/**
+	 * Pad a GPU image to the next supported FFT size using mirror out of bounds strategy.  
+	 * Note.  This function is not fully implemented on the GPU, thus the image is pulled 
+	 * onto the CPU, extended, then pushed back to GPU
+	 *
+	 * TODO fully implement this on the GPU
+	 * 
+	 * @param clij2
+	 * @param input
+	 * @param psf
+	 * @param ops
+	 * @return
+	 */
+	public static ClearCLBuffer padFFTInputMirror(CLIJ2 clij2, ClearCLBuffer input, ClearCLBuffer psf, OpService ops) {
+		
+		RandomAccessibleInterval img = clij2.pullRAI(input);
 
+		// compute extended dimensions based on image and PSF dimensions
+		long[] extendedSize = new long[img.numDimensions()];
+
+		for (int d = 0; d < img.numDimensions(); d++) {
+			extendedSize[d] = img.dimension(d) + psf.getDimensions()[d];
+		}
+
+		FinalDimensions extendedDimensions = new FinalDimensions(extendedSize);
+
+		// extend image
+		RandomAccessibleInterval extended = (RandomAccessibleInterval) ops.run(DefaultPadInputFFT.class, img, extendedDimensions, false,
+						new OutOfBoundsMirrorFactory(OutOfBoundsMirrorFactory.Boundary.SINGLE));
+	
+		// push extended image and psf to GPU
+		return  clij2.push(extended);
+		
+	}
+	
+	/**
+	 * Pad a GPU image to the next supported FFT size using zero padding
+	 * @param clij2
+	 * @param input
+	 * @param psf
+	 * @return
+	 */
+	public static ClearCLBuffer padFFTInputZeros(CLIJ2 clij2, ClearCLBuffer input, ClearCLBuffer psf)
+	{
+		
+		// calculate the next supported FFT size
+		int[] paddedForFFTSize = new int[3];
+		
+		for (int i = 0; i < 3; i++) {
+			long temp = input.getDimensions()[i]+psf.getDimensions()[i];
+			paddedForFFTSize[i] = NextSmoothNumber.nextSmooth((int)temp);
+		}
+		
+		ClearCLBuffer padded = clij2.create(paddedForFFTSize[0], paddedForFFTSize[1], paddedForFFTSize[2]);
+		
+		int startX = (paddedForFFTSize[0]-(int)input.getDimensions()[0])/2;
+		int startY = (paddedForFFTSize[1]-(int)input.getDimensions()[1])/2;
+		int startZ = (paddedForFFTSize[2]-(int)input.getDimensions()[2])/2;
+	
+		double average = clij2.getMeanOfAllPixels(input);
+		
+		clij2.set(padded, 1.);
+		clij2.paste(input, padded, startX, startY, startZ);
+		
+		return padded;
+	}
+	
+
+	/**
+	 * crop an image that has been extended for FFT back to original size
+	 * 
+	 * @param clij2
+	 * @param extended - the extended image
+	 * @param cropped - cropped, should be same size as the original image the extended image will be cropped to this size
+	 */
+	public static void cropExtended(CLIJ2 clij2, ClearCLBuffer extended, ClearCLBuffer cropped)
+	{
+		
+		long startX = (extended.getDimensions()[0]-(int)cropped.getDimensions()[0])/2;
+		long startY = (extended.getDimensions()[1]-(int)cropped.getDimensions()[1])/2;
+		long startZ = (extended.getDimensions()[2]-(int)cropped.getDimensions()[2])/2;
+		
+	  clij2.crop(extended, cropped, startX, startY, startZ);	
+		
+	}
+	
+	/**
+	 * pad a kernel (PSF) and move origin to center.  This needs to be done as a pre-processing step before 
+	 * calling FFT based convolution and/or deconvolution
+	 * 
+	 * @param clij2
+	 * @param convolution_kernel - the psf
+	 * @param extendedKernel - the extended and shifted psf will be written here
+	 * @return
+	 */
 	public static ClearCLBuffer padShiftFFTKernel(CLIJ2 clij2, ClearCLBuffer convolution_kernel,
 									ClearCLBuffer extendedKernel)
 	{
@@ -198,6 +295,24 @@ public class OpenCLFFTUtility {
 
 		return extendedKernel;
 	}
+
+	/**
+	 * normalize a PSF so the sum of all voxels is 1.0
+	 * 
+	 * @param clij2
+	 * @param input
+	 * @param destination
+	 * @return
+	 */
+	public static boolean normalize(CLIJ2 clij2, ClearCLBuffer input, ClearCLBuffer destination)
+	{
+		double sum = clij2.getSumOfAllPixels(input);
+
+		clij2.multiplyImageAndScalar(input, destination, 1/sum);
+
+		return true;
+	}
+
 
 	/**
 	 * Moves a quadrant of an image stack in a corner by mirroring it
