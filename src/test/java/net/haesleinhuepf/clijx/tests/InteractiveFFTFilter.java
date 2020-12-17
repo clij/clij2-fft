080331439acff8e3c7d1a27e13055c4669f98fff
Brian Northan <bnorthan@gmail.com>
2020-12-16 19:31:26 -0500

    Explain organization of complex array

--- src/test/java/net/haesleinhuepf/clijx/tests/InteractiveFFTFilter.java
+++ src/test/java/net/haesleinhuepf/clijx/tests/InteractiveFFTFilter.java
@@ -83,15 +83,16 @@ public class InteractiveFFTFilter {
 				.getPeerPointer().getPointer())).getNativePointer();
 		long l_queue = ((NativePointerObject) (clij2.getCLIJ().getClearCLContext()
 				.getDefaultQueue().getPeerPointer().getPointer())).getNativePointer();
-		long l_device = ((NativePointerObject) clij2.getCLIJ().getClearCLContext()
-				.getDevice().getPeerPointer().getPointer()).getNativePointer();
 
 		clij2fftWrapper.fft2d_32f_lp(N0, N1, longPointerImg, longPointerFFT, l_context, l_queue);
 		
 		clij2.show(FFT, "FFT");
 
 		// apply a naive box filter to the image
-		// consider FFT is complex with N0/2+1 coefficients in X, and N1 in Y
+		// consider FFT is complex with N0/2+1 coefficients in X, and N1 in Y but stored as floats
+		// the complex numbers are stored in the loat array in interleaving format 
+		// that is (real1, imag1, real2, imag2,... realn, imagn)
+		// TODO in the future it would be nice if CLIJ supported a complex format
 	  clij2.drawBox(filter, 0, 0, N0/8, N1/16);	
 	  clij2.drawBox(filter, 0, 15*N1/16, N0/8, N1/16);	
 	  
