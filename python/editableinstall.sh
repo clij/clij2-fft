84f5c787e3abfe40775e26df6fd8361ce68c0e26
bnorthan <bnorthan@gmail.com>
2023-01-05 10:02:43 -0500

    Add script to install in editable mode
    This is needed because the native libraries don't seem to be copied to
    the correct spot when installing in editable mode.  Thus the script
    copies the native libraries to a location where they can be found in an
    Annaconda or miniconda environment.
    
    This script is meant to be run locally so the paths would need to be
    changed on each machine to reflect the location of the
    anaconda/miniconda installation and current environment.

---
+++ python/editableinstall.sh
@@ -0,0 +1,35 @@
+# installs in editable mode, copying the native libraries to the location they are needed
+
+KERNEL=(`uname -s | tr [A-Z] [a-z]`)
+ARCH=(`uname -m | tr [A-Z] [a-z]`)
+
+# when we are installing in editable mode 
+# we have to put the libraries somewhere where python can find them. For me
+# (using anaconda) that was anaconda3\envs\(environment)\Library\bin on windows
+# and /home/bnorthan/anaconda3/envs/(environment)/lib/ on linux
+# YOU WILL HAVE TO CHANGE THE BELOW PATHS FOR YOUR SYSTEM
+case $KERNEL in
+    linux)
+        echo 'linux'
+        cp -r ./lib/linux64/* /home/bnorthan/anaconda3/envs/tnia_deconware/lib/
+        ;;
+    darwin)
+        echo 'mac'
+        case $ARCH in
+        x86_64)
+            echo 'macosx-x86_84'
+            cp -r ./lib/macosx/* /home/bnorthan/anaconda3/envs/tnia_deconware/lib/
+            ;;
+        arm84)
+            echo 'macosx-arm64'
+            cp -r ./lib/macosx-arm64/* /home/bnorthan/anaconda3/envs/tnia_deconware/lib/
+            ;;
+        esac
+        ;;
+    *)
+    echo 'not linux or mac (probably Windows)'
+        cp -r ./lib/win64/* /c/Users/bnort/miniconda3/envs/decon-napari/Library/bin/
+    ;;
+esac
+
+pip install -e .
