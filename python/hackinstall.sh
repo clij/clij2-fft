fbce6caa2f106d582a087614dea4fe2ffdc43b6c
bnorthan <bnorthan@gmail.com>
2023-01-05 10:05:29 -0500

    Add some documentation to hackinstall
    Note:  The hackinstall script will probably be removed soon as it is
    replaced by a combination of pythonbuild.sh and editableinstall.sh.

--- python/hackinstall.sh
+++ python/hackinstall.sh
@@ -4,28 +4,32 @@
 # installs the clij2fft python package then
 # deletes the lib directory
 
+# script may also be a useful template to follow if trying to install on your system
+
+# first build native code
 echo 'building native code'
 cd ../native
 bash cppbuild.sh
 cd ../python
-# currently
 
+# now copy the libraries so that they are under the python directory 
 echo 'copying libs'
-
 mkdir lib
 cp -r ../lib/* lib
 
-# note if we are installing in editable mode 
-# we have to put the libraries somewhere where python can find them. For me
-# that was anaconda3\envs\napari-env\Library\bin on windows
-# and /home/bnorthan/anaconda3/envs/tnia_deconware/lib/ on linux
 KERNEL=(`uname -s | tr [A-Z] [a-z]`)
 ARCH=(`uname -m | tr [A-Z] [a-z]`)
 
+# note if we are installing in editable mode 
+# we have to put the libraries somewhere where python can find them. For me
+# (using anaconda) that was anaconda3\envs\napari-env\Library\bin on windows
+# and /home/bnorthan/anaconda3/envs/tnia_deconware/lib/ on linux
+# YOU WILL HAVE TO CHANGE THE BELOW PATHS FOR YOUR SYSTEM
 case $KERNEL in
     linux)
         echo 'linux'
         cp -r ./lib/linux64/* /home/bnorthan/anaconda3/envs/tnia_deconware/lib/
+	pip install -e .
         ;;
     darwin)
         echo 'mac'
@@ -33,19 +37,17 @@ case $KERNEL in
         x86_64)
             echo 'macosx-x86_84'
             cp -r ./lib/macosx/* /home/bnorthan/anaconda3/envs/tnia_deconware/lib/
+	    pip install -e .
             ;;
         arm84)
             echo 'macosx-arm64'
             cp -r ./lib/macosx-arm64/* /home/bnorthan/anaconda3/envs/tnia_deconware/lib/
+	    pip install -e .
             ;;
         esac
         ;;
     *)
     echo 'not linux'
-    cp -r ./lib/win64/* /c/Users/bnort/miniconda3/envs/tnia_deconware/Library/bin/
+    cp -r ./lib/win64/* /c/Users/bnort/miniconda3/envs/decon-napari/Library/bin/
     ;;
 esac
-
-pip install -e .
-#rm -rf lib
-
