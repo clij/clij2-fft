b2b0d9c0cc7609b3201dd0c42db2d2f9ea19015b
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2023-08-11 14:48:54 -0400

    fix brew path on x86 macos

--- native/clij2fft/cppbuild.sh
+++ native/clij2fft/cppbuild.sh
@@ -37,7 +37,7 @@ case $PLATFORM in
                -DCMAKE_INSTALL_PREFIX="../../../lib/macosx/" \
                -DCMAKE_CXX_COMPILER="g++" \
                -DCMAKE_CUDA_HOST_COMPILER="g++" \
-		       -DCLFFT_LIBRARY_DIR="/opt/homebrew/opt/clfft/lib" ..
+		       -DCLFFT_LIBRARY_DIR="/usr/local/opt/clfft/lib" ..
         make
         make install
         ;;
