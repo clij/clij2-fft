ac00d8cb857ecedc2520e21c93dda13078fbeaa1
Brian Northan <bnorthan@gmail.com>
2023-10-19 12:58:19 -0400

    bump version

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='clij2-fft',
-      version='0.21',
+      version='0.22',
       description='A python wrapper around clij2 opencl FFT algorithms',
       long_description=long_description,
       long_description_content_type="text/markdown",
