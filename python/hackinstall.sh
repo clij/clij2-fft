9e824da63bc05a5a8eb9448dacac3addcc2d2951
bnorthan <bnorthan@gmail.com>
2023-12-04 08:06:27 -0500

    Make script a bit more DRY

--- python/hackinstall.sh
+++ python/hackinstall.sh
@@ -17,37 +17,4 @@ echo 'copying libs'
 mkdir lib
 cp -r ../lib/* lib
 
-KERNEL=(`uname -s | tr [A-Z] [a-z]`)
-ARCH=(`uname -m | tr [A-Z] [a-z]`)
-
-# note if we are installing in editable mode 
-# we have to put the libraries somewhere where python can find them. For me
-# (using anaconda) that was anaconda3\envs\napari-env\Library\bin on windows
-# and /home/bnorthan/anaconda3/envs/tnia_deconware/lib/ on linux
-# YOU WILL HAVE TO CHANGE THE BELOW PATHS FOR YOUR SYSTEM
-case $KERNEL in
-    linux)
-        echo 'linux'
-        cp -r ./lib/linux64/* /home/bnorthan/anaconda3/envs/tnia_deconware/lib/
-	pip install -e .
-        ;;
-    darwin)
-        echo 'mac'
-        case $ARCH in
-        x86_64)
-            echo 'macosx-x86_84'
-            cp -r ./lib/macosx/* /home/bnorthan/anaconda3/envs/tnia_deconware/lib/
-	    pip install -e .
-            ;;
-        arm84)
-            echo 'macosx-arm64'
-            cp -r ./lib/macosx-arm64/* /home/bnorthan/anaconda3/envs/tnia_deconware/lib/
-	    pip install -e .
-            ;;
-        esac
-        ;;
-    *)
-    echo 'not linux'
-    cp -r ./lib/win64/* /c/Users/bnort/miniconda3/envs/decon_bioformats/Library/bin/
-    ;;
-esac
+bash editableinstall.sh
