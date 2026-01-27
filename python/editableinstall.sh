561cf8a863f25f3c94f9529023d55519bf26b307
Brian Northan <bnorthan@gmail.com>
2026-01-27 11:05:51 -0500

    Improve comments

--- python/editableinstall.sh
+++ python/editableinstall.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# This script performs an editable install of the python package then
+# copies the required libraries into the conda environment.
+
 pip install -e .
 bash copylibsconda.sh
--- python/pythonbuild.sh
+++ python/pythonbuild.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# this script builds the native code then copies it to the python directory
-# so we can then upload to testpypi or pypi.
-# NOTE:  When uploading from one OS need to also build the native code on the other OSs then
-# move all (can do this via git) to the machine that you are uploading from
+# This script builds the native code and copies it to the python directory
+# so it can be uploaded to TestPyPI or PyPI.
+#
+# NOTE: When uploading from one OS, you need to also build the native code on the other OSs,
+# then move all libraries (via git) to the machine you are uploading from.
 
-# first build native code
-echo 'building native code'
+# First, build the native code
+echo 'Building native code...'
 cd ../native
 bash cppbuild.sh
 cd ../python
 
-# now copy the libraries so that they are under the python directory 
-# (remember only ones specific to this OS will have been built above, need to build push, and pull the others from git)
-echo 'copying libs'
+# Now copy the libraries into the python directory.
+# (Note: Only libraries specific to this OS will have been built above.
+#  You need to build on other platforms, commit, and pull those libraries from git.)
+echo 'Copying libraries...'
 mkdir lib
 cp -r ../lib/* lib
