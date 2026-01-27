89a0f0995626b3bd2df56f92acada6da8be45236
bnorthan <bnorthan@gmail.com>
2026-01-27 10:45:43 -0500

    Automatically detect which environment we are in
    This script is used to copy the native libraries to the current
    environment.  Previously we hard coded the environment location, but now
    we auto-detect the current environment name and copy to the correct
    location.

--- python/copylibsconda.sh
+++ python/copylibsconda.sh
@@ -1,37 +1,69 @@
 #!/bin/bash
-# this script is used when installing in editable mode, it copies the native libraries to the 
-# location they are needed NOTE this script needs to be customized for your system and environment 
-# name
+# This script automatically detects the active conda/mamba environment and copies 
+# native libraries to the appropriate location for editable mode installation.
 
 KERNEL=(`uname -s | tr [A-Z] [a-z]`)
 ARCH=(`uname -m | tr [A-Z] [a-z]`)
 
-# when we are installing in editable mode 
-# we have to put the libraries somewhere where python can find them. For me
-# (using anaconda) that was anaconda3\envs\(environment)\Library\bin on windows
-# and /home/bnorthan/anaconda3/envs/(environment)/lib/ on linux
-# YOU WILL HAVE TO CHANGE THE BELOW PATHS FOR YOUR SYSTEM
+# Detect active conda environment
+if [ -z "$CONDA_PREFIX" ]; then
+    echo "Error: No conda/mamba environment is active. Please activate an environment first."
+    exit 1
+fi
+
+echo "Detected active environment: $CONDA_PREFIX"
+
+# Determine target directory based on OS
 case $KERNEL in
     linux)
-        echo 'linux'
-        cp -r ./lib/linux64/* /home/bnorthan/mambaforge/envs/decon-dl-env2/lib/
+        echo "OS: Linux"
+        TARGET_DIR="$CONDA_PREFIX/lib"
+        SOURCE_DIR="./lib/linux64"
         ;;
     darwin)
-        echo 'mac'
+        echo "OS: macOS ($ARCH)"
+        TARGET_DIR="$CONDA_PREFIX/lib"
         case $ARCH in
-        x86_64)
-            echo 'macosx-x86_84'
-            cp -r ./lib/macosx/* /home/bnorthan/anaconda3/envs/tnia_deconware/lib/
-            ;;
-        arm84)
-            echo 'macosx-arm64'
-            cp -r ./lib/macosx-arm64/* /home/bnorthan/anaconda3/envs/tnia_deconware/lib/
-            ;;
+            x86_64)
+                SOURCE_DIR="./lib/macosx"
+                ;;
+            arm64)
+                SOURCE_DIR="./lib/macosx-arm64"
+                ;;
+            *)
+                echo "Error: Unsupported macOS architecture: $ARCH"
+                exit 1
+                ;;
         esac
         ;;
+    mingw* | msys* | cygwin*)
+        echo "OS: Windows"
+        TARGET_DIR="$CONDA_PREFIX/Library/bin"
+        SOURCE_DIR="./lib/windows-x86_64"
+        ;;
     *)
-    echo 'not linux or mac (probably Windows)'
-        cp -r ./lib/win64/* /c/Users/bnort/miniconda3/envs/decon_env_ts_python311/Library/bin/
-    ;;
+        echo "Error: Unsupported OS: $KERNEL"
+        exit 1
+        ;;
 esac
+
+# Verify source directory exists
+if [ ! -d "$SOURCE_DIR" ]; then
+    echo "Error: Source directory not found: $SOURCE_DIR"
+    exit 1
+fi
+
+# Create target directory if it doesn't exist
+mkdir -p "$TARGET_DIR"
+
+# Copy libraries
+echo "Copying libraries from $SOURCE_DIR to $TARGET_DIR"
+cp -r "$SOURCE_DIR"/* "$TARGET_DIR/"
+
+if [ $? -eq 0 ]; then
+    echo "Libraries copied successfully!"
+else
+    echo "Error: Failed to copy libraries"
+    exit 1
+fi
 
